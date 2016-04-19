7cd2bd36495cce730c978b1e90ee0a13461d70e2
Marcus <marcus.loo.vergara@gmail.com>
2016-04-19 19:50:54 +0200

    Test sample

--- samples/Testing/Source/Main.cpp
+++ samples/Testing/Source/Main.cpp
@@ -42,58 +42,138 @@ public:
 	}
 };
 
+#define WORLD_HEIGHT 10
+#define WORLD_WIDTH 10
+#define TILE_WIDTH 120
+#define TILE_HEIGHT 60
+
 class Testing : public Game
 {
-	float x, prevX;
-	bool move;
+	Resource<Texture2D> tileTexture, buildingTexture, unitATexture, unitBTexture;
+	StaticVertexBuffer *tileVBO;
+	StaticIndexBuffer *tileIBO;
 
 public:
 	Testing() :
-		Game("Testing"),
-		x(0.0f),
-		prevX(0.0f),
-		move(false)
+		Game("Testing")
 	{
 		setFlags(CGF_RUN_IN_BACKGROUND);
 	}
 
 	void onStart(GameEvent *)
 	{
-		getWindow()->setSize(800, 600);
-	}
+		tileTexture = getResourceManager()->get<Texture2D>("Tile");
+		buildingTexture = getResourceManager()->get<Texture2D>("Building");
+		unitATexture = getResourceManager()->get<Texture2D>("UnitA");
+		unitBTexture = getResourceManager()->get<Texture2D>("UnitB");
 
-	void onKeyDown(KeyEvent *e)
-	{
-		move = !move;
+		Vertex *vertices = new Vertex[WORLD_WIDTH * WORLD_HEIGHT * 4];
+		uint *indices = new uint[WORLD_WIDTH * WORLD_HEIGHT * 6];
+		for(int i = 0; i < WORLD_HEIGHT * WORLD_WIDTH; i++)
+		{
+			int y = i / WORLD_HEIGHT;
+			int x = i - y * WORLD_HEIGHT;
+
+			float xf = x + (y % 2 == 0 ? 0.0f : 0.5f);
+			float yf = y * 0.5f;
+
+			vertices[i * 4 + 0].set2f(VERTEX_POSITION, xf,     yf);
+			vertices[i * 4 + 1].set2f(VERTEX_POSITION, xf + 1, yf);
+			vertices[i * 4 + 2].set2f(VERTEX_POSITION, xf,     yf + 1);
+			vertices[i * 4 + 3].set2f(VERTEX_POSITION, xf + 1, yf + 1);
+
+			vertices[i * 4 + 0].set2f(VERTEX_TEX_COORD, 0.0f, 0.0f);
+			vertices[i * 4 + 1].set2f(VERTEX_TEX_COORD, 1.0f, 0.0f);
+			vertices[i * 4 + 2].set2f(VERTEX_TEX_COORD, 0.0f, 1.0f);
+			vertices[i * 4 + 3].set2f(VERTEX_TEX_COORD, 1.0f, 1.0f);
+
+			vertices[i * 4 + 0].set4ub(VERTEX_COLOR, 255, 255, 255, 255);
+			vertices[i * 4 + 1].set4ub(VERTEX_COLOR, 255, 255, 255, 255);
+			vertices[i * 4 + 2].set4ub(VERTEX_COLOR, 255, 255, 255, 255);
+			vertices[i * 4 + 3].set4ub(VERTEX_COLOR, 255, 255, 255, 255);
+
+			indices[i * 6 + 0] = i * 4 + 0;
+			indices[i * 6 + 1] = i * 4 + 2;
+			indices[i * 6 + 2] = i * 4 + 1;
+			indices[i * 6 + 3] = i * 4 + 1;
+			indices[i * 6 + 4] = i * 4 + 2;
+			indices[i * 6 + 5] = i * 4 + 3;
+		}
+
+		tileVBO = new StaticVertexBuffer(vertices, WORLD_WIDTH * WORLD_HEIGHT * 4);
+		tileIBO = new StaticIndexBuffer(indices, WORLD_WIDTH * WORLD_HEIGHT * 6);
 	}
 
 	void onTick(TickEvent *e)
 	{
-		if(move)
-		{
-			prevX = x;
-			x += 10.0f;
-			if(x > getWindow()->getWidth())
-			{
-				prevX = x = x - getWindow()->getWidth();
-			}
-		}
+
 	}
 
 	void onDraw(DrawEvent *e)
 	{
 		GraphicsContext *context = e->getGraphicsContext();
-		context->drawRectangle(math::lerp(prevX, x, e->getAlpha()) - 50, context->getHeight() * 0.5f - 50, 100, 100, Color(255));
+
+		// Draw tiles
+		Matrix4 tileMatrix;
+		tileMatrix.scale(TILE_WIDTH, TILE_HEIGHT, 1.0f);
+		context->pushMatrix(tileMatrix);
+		context->setTexture(tileTexture);
+		context->drawIndexedPrimitives(GraphicsContext::PRIMITIVE_TRIANGLES, tileVBO, tileIBO);
+		context->popMatrix();
+
+		// Draw building
+		{
+			int y = 3;
+			int x = 3;
+
+			float xf = x + (y % 2 == 0 ? 0.0f : 0.5f);
+			float yf = y * 0.5f;
+			context->setTexture(buildingTexture);
+			context->drawRectangle(xf*TILE_WIDTH, yf*TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT * 2);
+		}
+
+		{
+			int y = 3;
+			int x = 5;
+
+			float xf = x + (y % 2 == 0 ? 0.0f : 0.5f);
+			float yf = y * 0.5f;
+			context->setTexture(buildingTexture);
+			context->drawRectangle(xf*TILE_WIDTH, yf*TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT * 2);
+		}
+
+		// Draw units
+
+		{
+			int y = 1;
+			int x = 3;
+
+			float xf = x + (y % 2 == 0 ? 0.0f : 0.5f);
+			float yf = y * 0.5f;
+			context->setTexture(unitATexture);
+			context->drawRectangle(xf*TILE_WIDTH, yf*TILE_HEIGHT, TILE_WIDTH*3, TILE_HEIGHT * 4);
+		}
+
+
+		{
+			int y = 1;
+			int x = 3;
+
+			float xf = x + (y % 2 == 0 ? 0.0f : 0.5f);
+			float yf = y * 0.5f;
+			context->setTexture(unitBTexture);
+			context->drawRectangle(xf*TILE_WIDTH, yf*TILE_HEIGHT, TILE_WIDTH * 3, TILE_HEIGHT * 4);
+		}
 	}
 };
 
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, INT)
 {
 	// Testing
-	/*{
+	{
 		Testing game;
 		if(game.run() != CGF_OK) return EXIT_FAILURE;
-	}*/
+	}
 
 	// DrawTexturedQuad
 	{
