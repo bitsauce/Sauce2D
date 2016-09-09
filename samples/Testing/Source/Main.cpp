6849b07a5c50b7b8a01db4a99e6a2f4a1f99e70e
Marcus <marcus.loo.vergara@gmail.com>
2016-09-09 23:52:38 +0200

    Added GraphicsContext::getVertices

--- samples/Testing/Source/Main.cpp
+++ samples/Testing/Source/Main.cpp
@@ -43,7 +43,6 @@ public:
 	}
 };
 
-/*
 #define WORLD_HEIGHT 10
 #define WORLD_WIDTH 10
 #define TILE_WIDTH 120
@@ -76,57 +75,10 @@ public:
 
 	void onStart(GameEvent *)
 	{
-		getWindow()->setSize(1280, 720);
-
-		tileTexture = getResourceManager()->get<Texture2D>("Tile");
-		buildingTexture = getResourceManager()->get<Texture2D>("Building");
-		unitATexture = getResourceManager()->get<Texture2D>("UnitA");
-		unitBTexture = getResourceManager()->get<Texture2D>("UnitB");
-
-		buildingsShader = getResourceManager()->get<Shader>("BuildingsShader");
-		unitShader = getResourceManager()->get<Shader>("UnitShader");
-
-		Vertex *vertices = new Vertex[WORLD_WIDTH * WORLD_HEIGHT * 4];
-		uint *indices = new uint[WORLD_WIDTH * WORLD_HEIGHT * 6];
-		for(int i = 0; i < WORLD_HEIGHT * WORLD_WIDTH; i++)
-		{
-			int y = i / WORLD_HEIGHT;
-			int x = i - y * WORLD_HEIGHT;
-
-			float xf = x + (y % 2 == 0 ? 0.0f : 0.5f);
-			float yf = y * 0.5f;
-
-			vertices[i * 4 + 0].set2f(VERTEX_POSITION, xf,     yf);
-			vertices[i * 4 + 1].set2f(VERTEX_POSITION, xf + 1, yf);
-			vertices[i * 4 + 2].set2f(VERTEX_POSITION, xf,     yf + 1);
-			vertices[i * 4 + 3].set2f(VERTEX_POSITION, xf + 1, yf + 1);
-
-			vertices[i * 4 + 0].set2f(VERTEX_TEX_COORD, 0.0f, 0.0f);
-			vertices[i * 4 + 1].set2f(VERTEX_TEX_COORD, 1.0f, 0.0f);
-			vertices[i * 4 + 2].set2f(VERTEX_TEX_COORD, 0.0f, 1.0f);
-			vertices[i * 4 + 3].set2f(VERTEX_TEX_COORD, 1.0f, 1.0f);
-
-			vertices[i * 4 + 0].set4ub(VERTEX_COLOR, 255, 255, 255, 255);
-			vertices[i * 4 + 1].set4ub(VERTEX_COLOR, 255, 255, 255, 255);
-			vertices[i * 4 + 2].set4ub(VERTEX_COLOR, 255, 255, 255, 255);
-			vertices[i * 4 + 3].set4ub(VERTEX_COLOR, 255, 255, 255, 255);
-
-			indices[i * 6 + 0] = i * 4 + 0;
-			indices[i * 6 + 1] = i * 4 + 2;
-			indices[i * 6 + 2] = i * 4 + 1;
-			indices[i * 6 + 3] = i * 4 + 1;
-			indices[i * 6 + 4] = i * 4 + 2;
-			indices[i * 6 + 5] = i * 4 + 3;
-		}
-
-		tileVBO = new StaticVertexBuffer(vertices, WORLD_WIDTH * WORLD_HEIGHT * 4);
-		tileIBO = new StaticIndexBuffer(indices, WORLD_WIDTH * WORLD_HEIGHT * 6);
 	}
 
 	void onWindowSizeChanged(WindowEvent *e)
 	{
-		buildingsRT = new RenderTarget2D(e->getWidth(), e->getHeight());
-		unitsRT = new RenderTarget2D(e->getWidth(), e->getHeight());
 	}
 
 	void onTick(TickEvent *e)
@@ -136,104 +88,53 @@ public:
 
 	void onKeyEvent(KeyEvent *e)
 	{
-		if(e->getType() != KeyEvent::DOWN) return;
-		if(e->getScancode() == SAUCE_SCANCODE_1) showBuildings = !showBuildings;
-		else if(e->getScancode() == SAUCE_SCANCODE_2) showUnits = !showUnits;
-		else if(e->getScancode() == SAUCE_SCANCODE_3) showTiles = !showTiles;
 	}
-
-	void drawBuildings(GraphicsContext *context)
-	{
-		context->setRenderTarget(buildingsRT);
-
-		if(getInputManager()->getKeyState(SAUCE_SCANCODE_Q)) context->setShader(buildingsShader);
-
-		// Draw buildings
-		{
-			int y = 3;
-			int x = 3;
-
-			float xf = x + (y % 2 == 0 ? 0.0f : 0.5f);
-			float yf = y * 0.5f;
-			context->setTexture(buildingTexture);
-			buildingsShader->setSampler2D("u_Texture", buildingTexture);
-			context->drawRectangle(xf * TILE_WIDTH, yf * TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT * 2);
-		}
-
-		{
-			int y = 3;
-			int x = 5;
-
-			float xf = x + (y % 2 == 0 ? 0.0f : 0.5f);
-			float yf = y * 0.5f;
-			context->setTexture(buildingTexture);
-			context->drawRectangle(xf * TILE_WIDTH, yf * TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT * 2);
-		}
-
-		context->setShader(0);
-
-		context->setRenderTarget(0);
-
-		context->setTexture(buildingsRT->getTexture());
-		context->drawRectangle(0, 0, buildingsRT->getWidth(), buildingsRT->getHeight());
-	}
-
+	
 	void onDraw(DrawEvent *e)
 	{
 		GraphicsContext *context = e->getGraphicsContext();
 
-		// Draw tiles
-		Matrix4 tileMatrix;
-		tileMatrix.scale(TILE_WIDTH, TILE_HEIGHT, 1.0f);
-		context->pushMatrix(tileMatrix);
-		context->setTexture(tileTexture);
-		context->drawIndexedPrimitives(GraphicsContext::PRIMITIVE_TRIANGLES, tileVBO, tileIBO);
-		context->popMatrix();
+		const uint segments = 4;
 
-		if(showBuildings)
+		Vertex *vertices = context->getVertices(segments + 2);
+
+		float x = 100, y = 500;
+
+		vertices[0].set2f(VERTEX_POSITION, x, y);
+		vertices[0].set4ub(VERTEX_COLOR, 255, 255, 255, 255);
+		vertices[0].set2f(VERTEX_TEX_COORD, 0.5f, 0.5f);
+
+		float angle = 0.0f;
+
+		float f = angle - PI / 4.0f;
+
+		for(uint i = 1; i < segments + 2; ++i)
 		{
-			drawBuildings(e->getGraphicsContext());
+			vertices[i].set2f(VERTEX_POSITION, x + cos(f) * 500, y + sin(f) * 500);
+			vertices[i].set4ub(VERTEX_COLOR, 0, 0, 0, 0);
+			vertices[i].set2f(VERTEX_TEX_COORD, (1.0f + cos(f)) / 2.0f, (1.0f + sin(f)) / 2.0f);
+
+			f += (PI / 2.0f) / (segments + 2);
 		}
 
-		// Draw units
-		{
-			int y = 1;
-			int x = 3;
-
-			float xf = x + (y % 2 == 0 ? 0.0f : 0.5f);
-			float yf = y * 0.5f;
-			context->setTexture(unitATexture);
-			context->drawRectangle(xf*TILE_WIDTH, yf*TILE_HEIGHT, TILE_WIDTH*3, TILE_HEIGHT * 4);
-		}
-
-
-		{
-			int y = 1;
-			int x = 3;
-
-			float xf = x + (y % 2 == 0 ? 0.0f : 0.5f);
-			float yf = y * 0.5f;
-			context->setTexture(unitBTexture);
-			context->drawRectangle(xf*TILE_WIDTH, yf*TILE_HEIGHT, TILE_WIDTH * 3, TILE_HEIGHT * 4);
-		}
+		context->drawPrimitives(GraphicsContext::PRIMITIVE_TRIANGLE_FAN, &vertices[0], segments + 2);
 	}
 };
-*/
+
 
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, INT)
 {
 
 	// DrawTexturedQuad
-	{
+	/*{
 		DrawTexturedQuad game;
 		if(game.run() != SAUCE_OK) return EXIT_FAILURE;
-	}
+	}*/
 
-	// Isometric
-	/*{
+	{
 		Testing game;
 		if(game.run() != SAUCE_OK) return EXIT_FAILURE;
-	}*/
+	}
 
 	return EXIT_SUCCESS;
 }
