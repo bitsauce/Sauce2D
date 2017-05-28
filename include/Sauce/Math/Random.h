f3442b843d42af5aa6f0d2e7105c6f37a8d94255
Marcus <marcus.loo.vergara@gmail.com>
2017-05-28 21:44:14 +0200

    XORHash added

--- include/Sauce/Math/Random.h
+++ include/Sauce/Math/Random.h
@@ -1,7 +1,7 @@
 #ifndef SAUCE_RANDOM_H
 #define SAUCE_RANDOM_H
 
-#include <Sauce/config.h>
+#include <Sauce/Config.h>
 
 BEGIN_SAUCE_NAMESPACE
 
@@ -9,28 +9,82 @@ class SAUCE_API Random
 {
 public:
 	Random() :
+		m_seed((uint32_t) time(0))
+	{
+	}
+
+	Random(const uint32_t seed) :
+		m_seed(seed)
+	{
+	}
+
+	void setSeed(const uint32_t seed);
+	uint32_t getSeed() const { return m_seed; }
+
+	double nextDouble();
+	double nextDouble(const double max);
+	double nextDouble(const double min, const double max);
+
+	uint32_t nextInt();
+	uint32_t nextInt(const uint32_t max);
+	uint32_t nextInt(const uint32_t min, const uint32_t max);
+
+	bool chance(uint a, uint b); // has an 'a' out of 'b' chance to return true
+
+private:
+	uint32_t m_seed;
+};
+
+class XORHash
+{
+public:
+	XORHash() :
 		m_seed((uint32_t)time(0))
 	{
 	}
 
-	void setSeed(uint32_t seed);
-	uint32_t getSeed() const { return m_seed; }
+	XORHash(const uint32_t seed) :
+		m_seed(seed)
+	{
+	}
 
-	double nextDouble();
-	double nextDouble(double max);
-	double nextDouble(double min, double max);
+	template <typename Head, typename ...Tail>
+	uint32_t hash(Head head, Tail&&... tail)
+	{
+		head += m_seed;
+		head ^= head << 13;
+		head ^= head >> 17;
+		head ^= head << 5;
+		head *= 314159265;
+		return xorhash_internal(head, std::forward<Tail>(tail)...) ^ head;
+	}
 
-	uint32_t nextInt();
-	uint32_t nextInt(uint32_t max);
-	uint32_t nextInt(uint32_t min, uint32_t max);
-
-	double getDouble(uint32_t offset) const;
-	uint32_t getInt(uint32_t offset) const;
-
-	bool chance(uint a, uint b); // gives a out of b chance to return true
+	template <typename ...List>
+	double prob(List&&... tail)
+	{
+		return hash(std::forward<List>(tail)...) / 4294967295.0;
+	}
 
 private:
-	uint32_t m_seed;
+	template <typename Head>
+	uint32_t xorhash_internal(Head)
+	{
+		return 0;
+	}
+
+	template <typename Head, typename ...Tail>
+	uint32_t xorhash_internal(Head prevHead, Head head, Tail&&... tail)
+	{
+		head += m_seed;
+		head ^= prevHead;
+		head ^= head << 13;
+		head ^= head >> 17;
+		head ^= head << 5;
+		head *= 314159265;
+		return xorhash_internal(head, std::forward<Tail>(tail)...) ^ head;
+	}
+
+	const uint32_t m_seed;
 };
 
 END_SAUCE_NAMESPACE
